e13e093d5a0a6a261659cdd4978baccc22890b9b
EliasOjuara <araujoelias616@gmail.com>
2025-02-18 12:30:50 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using Exo.WebApi.Contexts;
+using Exo.WebApi.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<ExoContext, ExoContext>();
+builder.Services.AddControllers();
+builder.Services.AddTransient<ProjetoRepository, ProjetoRepository>();
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
